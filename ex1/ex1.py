c9810db7661dad8beae50f8cbdcdfe67640e2802
JmSeason <JmYangSeason@hotmail.com>
2017-03-31 18:18:50 +0800

    提交划分模块后的改进

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -7,17 +7,7 @@ Created on Wed Mar 29 10:29:21 2017
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-def load(file_name):
-    with open(file_name, 'r') as file:
-        dataset = []
-        for line in file:
-            eles = line.split(',')
-            if len(eles) < 1:
-                continue
-            eles = list(map(float, eles))
-            dataset.append(eles)
-    return np.array(dataset)
+import excommon as com
     
 def compute_cost(X, y, theta):
     m = len(y)
